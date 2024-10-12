a75cd1c829b3ac4f9e8f43d6bd7be23639ff678e
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-12 13:01:00 -0500

    updated route to post, added show action/view, display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "products#index"
 
+  get "/products", to: "products#index"
+  get "/products/:id", to: "products#show"
+
   resources :products
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
