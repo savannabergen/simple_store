5d45e013957ae14bb515bedeb73baf671cb66354
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-12 14:42:45 -0500

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :category
+
   validates :title, presence: { message: "cannot be blank" }
   validates :price, presence: { message: "cannot be blank" }
   validates :stock_quantity, presence: { message: "cannot be blank" }
