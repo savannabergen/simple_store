8ba4822462779763784486aea282df1eba9ab615
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-13 11:30:31 -0500

    seeds file updated, data inserted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-require 'faker'
+require "csv"
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 5, to: 500)
-  )
-end
+csv_file = Rails.root.join('db/products.csv')
+   csv_data = File.read(csv_file)
+
+   products = CSV.parse(csv_data, headers: true)
+
+   # If CSV was created by Excel in Windows you may also need to set an encoding type:
+   # products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+   products.each do |product|
+    category_name = product['category']
+    category = Category.find_or_create_by(name: category_name)
+
+    Product.create(
+      title: product['name'],
+      price: product['price'],
+      description: product['description'],
+      stock_quantity: product['stock quantity'],
+      category: category
+    )
+  end
